294eeea42bcd62efb9873d18db2ffe0ddd05bdfd
Helen Guan <helenguan30@gmail.com>
2023-02-12 13:43:13 -0500

    Change conditions layout

--- backend/python/app/rest/logs_routes.py
+++ backend/python/app/rest/logs_routes.py
@@ -22,22 +22,22 @@ def filter_logs():
     start_date = request.json["start_date"]
     end_date = request.json["end_date"]
 
-    if email and (start_date and end_date):
-        start_date = datetime.strptime(start_date, "%Y-%m-%d")
-        end_date = datetime.strptime(end_date, "%Y-%m-%d").replace(hour=23, minute=59)
-        if type(email) is not str or (
-            type(start_date) is not datetime or type(end_date) is not datetime
-        ):
+    if email and type(email) is not str:
+        return jsonify({"error": "email query parameter must be a string"}), 400
+    
+    if start_date and end_date:
+        try: 
+            start_date = datetime.strptime(start_date, "%Y-%m-%d")
+            end_date = datetime.strptime(end_date, "%Y-%m-%d").replace(hour=23, minute=59)
+        except Exception as e:
             return (
-                jsonify(
-                    {
-                        "error": "start_date and end_date query parameter must be datetime and email must be a string"
-                    }
-                ),
-                400,
+                jsonify({"error": "Date is not in correct format"}),
+                500,
             )
-        elif start_date > end_date:
-            return jsonify({"error": "start_date must be before end_dateg"}), 400
+        if start_date > end_date: 
+            return jsonify({"error": "start_date must be before end"}), 400
+
+    if email and start_date and end_date:
         try:
             user_id = user_service.get_user_by_email(email).id
             logs = sign_in_logs_service.get_logs_by_date_range_and_id(
@@ -51,36 +51,19 @@ def filter_logs():
                 jsonify({"error": (error_message if error_message else str(e))}),
                 500,
             )
-    elif email and not (start_date or end_date):
-        if type(email) is not str:
-            return jsonify({"error": "email query parameter must be a string"}), 400
-        else:
-            try:
-                user_id = user_service.get_user_by_email(email).id
-                logs = sign_in_logs_service.get_logs_by_id(user_id)
-                logs_list = sign_in_logs_service.get_logs_list(logs)
-                return logs_list, 200
-            except Exception as e:
-                error_message = getattr(e, "message", None)
-                return (
-                    jsonify({"error": (error_message if error_message else str(e))}),
-                    500,
-                )
-    elif start_date and end_date:
-        start_date = datetime.strptime(start_date, "%Y-%m-%d")
-        end_date = datetime.strptime(end_date, "%Y-%m-%d").replace(hour=23, minute=59)
-
-        if type(start_date) is not datetime or type(end_date) is not datetime:
+    elif email and not (start_date and end_date):
+        try:
+            user_id = user_service.get_user_by_email(email).id
+            logs = sign_in_logs_service.get_logs_by_id(user_id)
+            logs_list = sign_in_logs_service.get_logs_list(logs)
+            return logs_list, 200
+        except Exception as e:
+            error_message = getattr(e, "message", None)
             return (
-                jsonify(
-                    {
-                        "error": "start_date and end_date query parameter must be datetime"
-                    }
-                ),
-                400,
+                jsonify({"error": (error_message if error_message else str(e))}),
+                500,
             )
-        elif start_date > end_date:
-            return jsonify({"error": "start_date must be before end_dateg"}), 400
+    elif start_date and end_date:
         try:
             # return as json object
             logs = sign_in_logs_service.get_logs_by_date_range(start_date, end_date)
