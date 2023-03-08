d4173ad1432128930a5ab6aac4e49c64a83fcd6e
Safewaan <safwaanchowdhury26@gmail.com>
2023-03-07 20:33:26 -0500

    add styling, reposition navbar to the top

--- frontend/src/components/common/NavigationBar.tsx
+++ frontend/src/components/common/NavigationBar.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import {
   Box,
   Flex,
-  Avatar,
   HStack,
   Link,
   IconButton,
+  Image,
   Button,
   Menu,
   MenuButton,
@@ -17,14 +17,28 @@ import {
   Stack,
 } from "@chakra-ui/react";
 
-const Links = ["Dashboard", "Projects", "Team"];
+import avatarImage from "../../images/avatar.png";
+import Logo from "../../images/Logo.png"
+
+type NavigationItemProps = {
+  onClick: () => void;
+  text: string;
+};
+
+const NavigationItem: React.FC<NavigationItemProps> = ({ onClick, text }: NavigationItemProps) => {
+  return (
+    <button type="button" onClick={onClick}>
+      {text}
+    </button>
+  );
+};
 
 const NavigationBar = (): React.ReactElement => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <div className="Navigation Bar">
-      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+      <Box position="fixed" top={0} left={0} right={0} bg='#285E61' px={4}>
         <Flex h={16} alignItems="center" justifyContent="space-between">
           <IconButton
             size="md"
@@ -38,11 +52,14 @@ const NavigationBar = (): React.ReactElement => {
               spacing={4}
               display={{ base: "none", md: "flex" }}
             />
+            <Image
+                  src={Logo}
+                />
+            <NavigationItem onClick={() => console.log('Home clicked')} text="Home" />
+            <NavigationItem onClick={() => console.log('About clicked')} text="Resident Directory" />
+            <NavigationItem onClick={() => console.log('Contact clicked')} text="Admin" />
           </HStack>
           <Flex alignItems="center">
-            <Button variant="solid" colorScheme="teal" size="sm" mr={4}>
-              Action
-            </Button>
             <Menu>
               <MenuButton
                 as={Button}
@@ -51,16 +68,14 @@ const NavigationBar = (): React.ReactElement => {
                 cursor="pointer"
                 minW={0}
               >
-                <Avatar
-                  size="sm"
-                  src="https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+                <Image
+                  borderRadius='full'
+                  boxSize='50px'
+                  src={avatarImage}
                 />
               </MenuButton>
               <MenuList>
-                <MenuItem>Link 1</MenuItem>
-                <MenuItem>Link 2</MenuItem>
-                <MenuDivider />
-                <MenuItem>Link 3</MenuItem>
+                <MenuItem>Sign Out</MenuItem>
               </MenuList>
             </Menu>
           </Flex>
