1b51fb29b589e198e36196413341bd64eef8569d
Kelly Pham <phamkelly17@gmail.com>
2023-03-12 14:01:13 -0400

    adding log records table and API endpoints

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -9,6 +9,7 @@ def init_app(app):
     from .entity import Entity
     from .user import User
     from .sign_in_logs import SignInLogs
+    from .log_records import LogRecords
 
     app.app_context().push()
     db.init_app(app)
---
+++ backend/app/models/log_records.py
@@ -0,0 +1,35 @@
+from . import db
+from sqlalchemy import inspect
+from sqlalchemy.orm.properties import ColumnProperty
+
+class LogRecords(db.Model):
+    __tablename__ = "log_records"
+    log_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    employee_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    # should i make a table with residents 
+    resident_first_name = db.Column(db.String, nullable=False)
+    resident_last_name = db.Column(db.String, nullable=False)
+    time = db.Column(db.DateTime(timezone=True), nullable=False)
+    flagged = db.Column(db.Boolean, nullable=False)
+    attn_to = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=True)
+    note = db.Column(db.String, nullable=True)
+
+    def to_dict(self, include_relationships=False):
+        # define the entities table
+        cls = type(self)
+
+        mapper = inspect(cls)
+        formatted = {}
+        for column in mapper.attrs:
+            field = column.key
+            attr = getattr(self, field)
+            # if it's a regular column, extract the value
+            if isinstance(column, ColumnProperty):
+                formatted[field] = attr
+            # otherwise, it's a relationship field
+            # (currently not applicable, but may be useful for entity groups)
+            elif include_relationships:
+                # recursively format the relationship
+                # don't format the relationship's relationships
+                formatted[field] = [obj.to_dict() for obj in attr]
+        return formatted
