a14a88a5d1e8ac58e4fa6db127677da436d6014d
Helen Guan <helenguan30@gmail.com>
2023-03-14 21:15:31 -0400

    Address comments and upgrade fields of table

--- backend/app/models/resident.py
+++ backend/app/models/resident.py
@@ -1,21 +1,22 @@
 from . import db
-import enum
 from sqlalchemy import inspect
 from sqlalchemy.orm.properties import ColumnProperty
 
-#- id
-#- date joined 
-#- status: 
-
-class StatusEnum(enum.Enum):
-    undetermined = "undetermined"
-
-
-class Residence(db.Model):
-    __tablename__ = "residences"
+class Resident(db.Model):
+    __tablename__ = "residents"
     id = db.Column(db.Integer, primary_key=True, nullable=False)
+    initial = db.Column(db.String, nullable=False)
+    room_num = db.Column(db.Integer, nullable=False)
     date_joined = db.Column(db.Date, nullable=False)
-    status = db.Column(db.Enum(StatusEnum), nullable=False)
+    status = db.Column(
+        db.Enum("Current", "Past", name="statuses"), nullable=False
+        )
+    building = db.Column(
+        db.Enum("Seniors", "Adults", "Supports", name="buildings"), nullable=False
+        )
+    address = db.Column(
+        db.Enum("144 Erb St. East", "402 Erb St. West", "362 Erb St. West", name="buildings"), nullable=True
+        )
 
     def to_dict(self, include_relationships=False):
         # define the entities table
