7a7c2dda7684002d173ceef9aa97adf4118c2cc3
Safewaan <Safewaan@users.noreply.github.com>
2023-05-09 18:59:24 -0400

    add error handling for several error cases in the form

--- frontend/src/components/forms/CreateLog.tsx
+++ frontend/src/components/forms/CreateLog.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable prettier/prettier */
 import React, { useState } from "react";
 import Select, { ActionMeta, MultiValue, SingleValue } from 'react-select'
 import {
     Button,
     Checkbox,
     FormControl,
+    FormErrorMessage,
     FormLabel,
     Grid,
     GridItem,
@@ -47,10 +49,22 @@ const EMPLOYEES = [
     { label: "John Doe", value: "John Doe" },
 ]
 
+// Changes the border of the Select components if the input is invalid
+function getBorderStyle(state: any, error: boolean): string {
+    if (state.isFocused) {
+        return "2px solid #3182ce";
+    }
+    if (error) {
+        return "2px solid #e53e3e";
+    }
+
+    return "1px solid #cbd5e0";
+}
+
 const CreateLog = () => {
     const [employee, setEmployee] = useState("Huseyin"); // currently, the select for employees is locked and should default to current user. Need to check if admins/regular staff are allowed to change this
     const [date, setDate] = useState(new Date());
-    const [time, setTime] = useState("");
+    const [time, setTime] = useState(date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }));
     const [building, setBuilding] = useState("");
     const [resident, setResident] = useState("");
     const [tags, setTags] = useState<string[]>([]);
@@ -65,6 +79,7 @@ const CreateLog = () => {
     const [residentError, setResidentError] = useState(false);
     const [notesError, setNotesError] = useState(false);
 
+    // if we need functionality to change the selected employee, handle should go here
 
     const handleDateChange = (newDate: Date) => {
         setDate(newDate);
@@ -82,18 +97,24 @@ const CreateLog = () => {
             updatedDate.setMinutes(parseInt(minute, 10));
             setDate(updatedDate);
         }
+
+        setTimeError(e.target.value === "");
     };
 
     const handleBuildingChange = (selectedOption: SingleValue<{ label: string; value: string; }>) => {
         if (selectedOption !== null) {
             setBuilding(selectedOption.value);
         }
+
+        setBuildingError(selectedOption === null);
     };
 
     const handleResidentChange = (selectedOption: SingleValue<{ label: string; value: string; }>) => {
         if (selectedOption !== null) {
             setResident(selectedOption.value);
         }
+
+        setResidentError(selectedOption === null);
     };
 
     const handleTagsChange = (selectedTags: MultiValue<{ label: string; value: string; }>) => {
@@ -110,6 +131,7 @@ const CreateLog = () => {
     const handleNotesChange = (e: { target: { value: unknown } }) => {
         const inputValue: string = e.target.value as string;
         setNotes(inputValue);
+        setNotesError(inputValue === "");
     };
 
     const [isCreateOpen, setCreateOpen] = React.useState(false);
@@ -119,21 +141,64 @@ const CreateLog = () => {
 
     const handleCreateClose = () => {
         setCreateOpen(false);
+
+        // reset all states
+        setDate(new Date());
+        setBuilding("");
+        setResident("");
+        setTags([]);
+        setAttnTo("");
+        setNotes("");
+        setCreateOpen(false);
+
+        // reset all error states
+        setEmployeeError(false);
+        setDateError(false);
+        setTimeError(false);
+        setBuildingError(false);
+        setResidentError(false);
+        setNotesError(false);
     };
 
     const handleSubmit = () => {
-        // Do something with the form data
+        // log the state values for testing
         console.log({
             employee,
             date,
+            time,
             building,
             resident,
             tags,
             attnTo,
             notes,
         });
+
+        // Check if required fields are filled out
+        setEmployeeError(employee === "");
+        setDateError(date === null);
+        setTimeError(time === "");
+        setBuildingError(building === "");
+        setResidentError(resident === "");
+        setNotesError(notes === "");
+
+        // log the error state values for testing
+        console.log({
+            employeeError: employee === "",
+            dateError: date === null,
+            timeError: time === "",
+            buildingError: building === "",
+            residentError: resident === "",
+            notesError: notes === "",
+        });
+
+        // If any required fields are empty, prevent form submission
+        if (employee === "" || date === null || time === "" || building === "" || resident === "" || notes === "") {
+            return;
+        }
+
+        // Create a log in the db with this data
+
         // Clear the form fields after submission
-        setEmployee("");
         setDate(new Date());
         setBuilding("");
         setResident("");
@@ -157,12 +222,20 @@ const CreateLog = () => {
                     <ModalBody>
                         <Row>
                             <Col>
-                                <FormControl isRequired isInvalid={employeeError}>
+                                <FormControl isRequired>
                                     <FormLabel>Employee</FormLabel>
                                     <Select
                                         options={EMPLOYEES}
                                         isDisabled
                                         defaultValue={{ label: employee, value: employee }} // needs to be the current user
+                                        styles={{
+                                            control: (provided, state) => ({
+                                                ...provided,
+                                                border: getBorderStyle(state, employeeError),
+
+                                                borderRadius: "4px"
+                                            })
+                                        }}
                                     />
                                 </FormControl>
                             </Col>
@@ -179,14 +252,15 @@ const CreateLog = () => {
                                         </FormControl>
                                     </GridItem>
                                     <GridItem>
-                                        <FormControl isRequired>
+                                        <FormControl isRequired isInvalid={timeError}>
                                             <FormLabel>Time</FormLabel>
                                             <Input
                                                 size="md"
                                                 type="time"
-                                                defaultValue={date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}
+                                                defaultValue={time}
                                                 onChange={handleTimeChange}
                                             />
+                                            <FormErrorMessage>Time is invalid.</FormErrorMessage>
                                         </FormControl>
                                     </GridItem>
                                 </Grid>
@@ -195,23 +269,47 @@ const CreateLog = () => {
 
                         <Row>
                             <Col>
-                                <FormControl isRequired mt={4}>
+                                <FormControl isRequired isInvalid={buildingError} mt={4}>
                                     <FormLabel>Building</FormLabel>
                                     <Select
                                         options={BUILDINGS}
                                         placeholder="Building No."
                                         onChange={handleBuildingChange}
+                                        styles={{
+                                            control: (provided, state) => ({
+                                                ...provided,
+                                                border: getBorderStyle(state, buildingError),
+                                                "&:hover": {
+                                                    borderColor: buildingError ? "#e53e3e" : "#B1B1B1",
+                                                    cursor: "pointer"
+                                                },
+                                                borderRadius: "4px"
+                                            })
+                                        }}
                                     />
+                                    <FormErrorMessage>Building is required.</FormErrorMessage>
                                 </FormControl>
                             </Col>
                             <Col>
-                                <FormControl isRequired mt={4}>
+                                <FormControl isRequired isInvalid={residentError} mt={4}>
                                     <FormLabel>Resident</FormLabel>
                                     <Select
                                         options={RESIDENTS}
                                         placeholder="Select Resident"
                                         onChange={handleResidentChange}
+                                        styles={{
+                                            control: (provided, state) => ({
+                                                ...provided,
+                                                border: getBorderStyle(state, residentError),
+                                                "&:hover": {
+                                                    borderColor: residentError ? "#e53e3e" : "#B1B1B1",
+                                                    cursor: "pointer"
+                                                },
+                                                borderRadius: "4px"
+                                            })
+                                        }}
                                     />
+                                    <FormErrorMessage>Resident is required.</FormErrorMessage>
                                 </FormControl>
                             </Col>
                         </Row>
@@ -243,14 +341,17 @@ const CreateLog = () => {
 
                         <Row>
                             <Col>
-                                <FormLabel mt={4}>Notes</FormLabel>
-                                <Textarea
-                                    value={notes}
-                                    onChange={handleNotesChange}
-                                    placeholder="Enter log notes here..."
-                                    size="lg"
-                                    style={{ resize: "none" }}
-                                />
+                                <FormControl isRequired isInvalid={notesError} mt={4}>
+                                    <FormLabel>Notes</FormLabel>
+                                    <Textarea
+                                        value={notes}
+                                        onChange={handleNotesChange}
+                                        placeholder="Enter log notes here..."
+                                        size="lg"
+                                        style={{ resize: "none" }}
+                                    />
+                                    <FormErrorMessage>Notes are required.</FormErrorMessage>
+                                </FormControl>
                             </Col>
                         </Row>
 
