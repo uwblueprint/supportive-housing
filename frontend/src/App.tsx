4817df997f20658d59c1a4813238c5a049a99632
Safewaan <Safewaan@users.noreply.github.com>
2023-05-11 18:22:02 -0400

    undo route component change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -59,7 +59,7 @@ const App = (): React.ReactElement => {
               <Switch>
                 <Route exact path={Routes.LOGIN_PAGE} component={Login} />
                 <Route exact path={Routes.SIGNUP_PAGE} component={Signup} />
-                <Route
+                <PrivateRoute
                   exact
                   path={Routes.HOME_PAGE}
                   component={LogRecords}
