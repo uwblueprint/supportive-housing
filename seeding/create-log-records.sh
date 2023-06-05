9287bf3ef5f10847f1325b87ed8370bd4665b9b8
Safewaan <Safewaan@users.noreply.github.com>
2023-06-05 16:46:59 -0400

    added script to seed user in db

---
+++ seeding/create-log-records.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Import common functions
+source ./seed.sh
+
+# Run SQL script in docker container
+run_sql_script "
+INSERT INTO log_records (
+  employee_id, 
+  resident_first_name, 
+  resident_last_name, 
+  datetime, 
+  flagged, 
+  attn_to, 
+  note, 
+  tags, 
+  building 
+) 
+SELECT
+  $EMPLOYEE_ID,
+  (ARRAY['John','Jerry','James','Jack'])[floor(random() * 4 + 1)], 
+  (ARRAY['Doe','Show','Flow','Mow'])[floor(random() * 4 + 1)], 
+  NOW() + (random() * (NOW()+'90 days' - NOW())) + '30 days', 
+  (ARRAY[true,false])[floor(random() * 2 + 1)], 
+  $ATTN_TO_ID, 
+  (ARRAY['Amazing note','This is a note','Bad note','Decent Note','Cool note'])[floor(random() * 5 + 1)], 
+  ARRAY['tag1', 'tag2'], 
+  (ARRAY['144', '362', '402'])[floor(random() * 3 + 1)]
+
+FROM generate_series(1, $LOG_RECORD_ROWS);
+"
+
+echo "Log Record seeding complete"
---
+++ seeding/create-user.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Import common functions
+source ./seed.sh
+
+# Run SQL script in docker container
+run_sql_script "
+INSERT INTO users (
+  first_name,
+  last_name,
+  auth_id,
+  role
+)
+SELECT 
+  '$FIRST_NAME',
+  '$LAST_NAME',
+  '$AUTH_ID',
+  '$ROLE'
+WHERE NOT EXISTS (
+  SELECT 1
+  FROM users
+  WHERE first_name = '$FIRST_NAME'
+    AND last_name = '$LAST_NAME'
+);
+"
+
+echo "User seeding complete"
