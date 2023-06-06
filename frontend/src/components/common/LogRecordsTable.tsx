abe857405fc634ffdecde9c6bc00293f6a4cd918
Kevin Pierce <kevinpierce08@gmail.com>
2023-06-05 21:45:41 -0400

    Resolved date formatting, added new date formatted util fn

--- frontend/src/components/common/LogRecordsTable.tsx
+++ frontend/src/components/common/LogRecordsTable.tsx
@@ -9,6 +9,7 @@ import {
   Tr,
 } from "@chakra-ui/react";
 import { LogRecord } from "../../types/LogRecordTypes";
+import getFormattedDateAndTime from '../../utils/DateUtils'
 
 type Props = {
   logRecords: LogRecord[];
@@ -39,13 +40,15 @@ const LogRecordsTable = ({ logRecords }: Props): React.ReactElement => {
               // TODO: Investigate alternative methods for date storage + creation
               const dateObj = new Date(record.datetime);
 
+              const {date, time} = getFormattedDateAndTime(dateObj)
+
               return (
                 <Tr key={record.logId} style={{ verticalAlign: "middle" }}>
                   <Td width="5%">
-                    {`${dateObj.getMonth()} ${dateObj.getDate()} ${dateObj.getFullYear()}`}
+                   {date}
                   </Td>
                   <Td width="5%">
-                    {`${dateObj.getHours()}:${dateObj.getMinutes()}`}
+                    {time}
                   </Td>
                   {
                     // TODO: Resolve the resident record at some point
