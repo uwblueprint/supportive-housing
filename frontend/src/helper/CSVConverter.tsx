9702be2e980a0e68c8bad202fad5a2c0e485eda2
owen-sellner <owen.sellner@gmail.com>
2023-06-22 13:12:10 -0400

    remove columns from CSV

--- frontend/src/helper/CSVConverter.tsx
+++ frontend/src/helper/CSVConverter.tsx
@@ -1,16 +1,33 @@
 import { LogRecord } from "../types/LogRecordTypes";
+import { CSVLog } from "../types/CSVLog";
+
+const convertToCSVLog = (logRecord: LogRecord): CSVLog => {
+  const attnTo = `${logRecord.attnToFirstName} ${logRecord.attnToLastName}`;
+  const employee = `${logRecord.employeeFirstName} ${logRecord.employeeLastName}`;
+  
+  return {
+    attnTo,
+    building: logRecord.building,
+    datetime: logRecord.datetime,
+    employee,
+    flagged: logRecord.flagged,
+    note: logRecord.note,
+    residentFirstName: logRecord.residentFirstName,
+    residentLastName: logRecord.residentLastName,
+    tags: logRecord.tags.join("; "),
+  };
+};
 
 const CSVConverter = (data: LogRecord[]): boolean => {
   // Convert JSON to CSV
   try {
     const csvRows = [];
 
-    const headers = Object.keys(data[0]);
+    const headers = ["attnTo", "building", "datetime", "employee", "flagged", "note", "residentFirstName", "residentLastName", "tags"];
     csvRows.push(headers.join(","));
     data.forEach((log: LogRecord) => {
-      const tags = log.tags.join("; ");
-      const CSVLog = { ...log, tags };
-      const values = Object.values(CSVLog).join(",");
+      const logCSV = convertToCSVLog(log); 
+      const values = Object.values(logCSV).join(",");
       csvRows.push(values);
     });
     const csvContent = csvRows.join("\n");
---
+++ frontend/src/types/CSVLog.ts
@@ -0,0 +1,11 @@
+export type CSVLog = {
+    attnTo: string;
+    building: string;
+    datetime: string;
+    employee: string;
+    flagged: boolean;
+    note: string;
+    residentFirstName: string;
+    residentLastName: string;
+    tags: string;
+};
