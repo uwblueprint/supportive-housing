ae4d47148807631accf5aed057970239a4cb582f
Kevin Pierce <kevinpierce08@gmail.com>
2023-07-12 09:01:01 -0400

    Final styling touchups

--- frontend/src/components/pages/ResidentDirectory/ResidentDirectory.tsx
+++ frontend/src/components/pages/ResidentDirectory/ResidentDirectory.tsx
@@ -1,28 +1,12 @@
 import React, { useState, useRef, useEffect } from "react";
-import { Box, Button, Flex, Spacer } from "@chakra-ui/react";
+import { Box, Flex, Spacer } from "@chakra-ui/react";
 import ResidentDirectoryTable from './ResidentDirectoryTable'
 import NavigationBar from "../../common/NavigationBar";
 import { Resident } from "../../../types/ResidentTypes";
 import ResidentAPIClient from "../../../APIClients/ResidentAPIClient";
 import Pagination from "../../common/Pagination";
 
-const SAMPLE_RESIDENTS: Resident[] = [
-  { id: 1, initial: 'AA', roomNum: 132, dateJoined: new Date(), building: "362"},
-  { id: 2, initial: 'BB', roomNum: 131, dateJoined: new Date(), dateLeft: new Date(), building: "362"},
-  { id: 3, initial: 'CC', roomNum: 231, dateJoined: new Date(), building: "362"},
-  { id: 4, initial: 'DD', roomNum: 131, dateJoined: new Date(), building: "362"},
-  { id: 5, initial: 'EE', roomNum: 113, dateJoined: new Date(), dateLeft: new Date(), building: "144"},
-  { id: 6, initial: 'EF', roomNum: 1, dateJoined: new Date(), building: "362"},
-  { id: 7, initial: 'FF', roomNum: 22, dateJoined: new Date(), building: "362"},
-  { id: 8, initial: 'HI', roomNum: 365, dateJoined: new Date(), dateLeft: new Date(), building: "402"},
-  { id: 9, initial: 'GG', roomNum: 14, dateJoined: new Date(), building: "362"},
-  { id: 10, initial: 'EZ', roomNum: 901, dateJoined: new Date(), building: "362"},
-  { id: 11, initial: 'WOW', roomNum: 13, dateJoined: new Date(), dateLeft: new Date(), building: "362"},
-  { id: 112, initial: 'CS', roomNum: 43, dateJoined: new Date(), building: "362"},
-]
-
 const ResidentDirectory = (): React.ReactElement => {
-  // Record/page state
   const [residents, setResidents] = useState<Resident[]>([]);
   const [numResidents, setNumResidents] = useState<number>(0);
   const [resultsPerPage, setResultsPerPage] = useState<number>(25);
@@ -71,10 +55,8 @@ const ResidentDirectory = (): React.ReactElement => {
         <Flex marginBottom="16px">
           <Box textStyle="header-large">Resident Directory</Box>
           <Spacer />
-          {/* TODO: INSERT RESIDENT-RELATED BUTTONS */}
           <Flex justify="end" gap="12px">
-            <Button>PlaceHolder1</Button>
-            <Button>PlaceHolder2</Button>
+          {/* TODO: INSERT RESIDENT-RELATED BUTTONS */}
           </Flex>
         </Flex>
         <ResidentDirectoryTable
