e6a9538f6cc49a628fd3af8921bb305a4420f35c
Braydon Wang <b392wang@uwaterloo.ca>
2023-09-24 15:58:00 -0400

    created get tag route

---
+++ backend/app/rest/tag_routes.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, current_app, jsonify
+from ..middlewares.auth import require_authorization_by_role
+from ..services.implementations.tags_service import TagsService
+
+tags_service = TagsService(current_app.logger)
+blueprint = Blueprint("tags", __name__, url_prefix="/tags")
+
+@blueprint.route("/", methods=["GET"], strict_slashes=False)
+# @require_authorization_by_role({"Relief Staff", "Regular Staff", "Admin"})
+def get_tags():
+    """
+    Get tags.
+    """
+    try:
+        tags_results = tags_service.get_tags()
+        return jsonify(tags_results), 200
+    except Exception as e:
+        error_message = getattr(e, "message", None)
+        return jsonify({"error": (error_message if error_message else str(e))}), 500
---
+++ backend/app/services/implementations/tags_service.py
@@ -0,0 +1,27 @@
+from ..interfaces.tags_service import ITagsService
+from ...models.tag import Tag
+
+class TagsService(ITagsService):
+    """
+    Tags implementation with tags management methods
+    """
+
+    def __init__(self, logger):
+        """
+        Create an instance of TagsService
+
+        :param logger: application's logger instance
+        :type logger: logger
+        """
+        self.logger = logger
+
+    def get_tags(self):
+        try:
+            tags_results = Tag.query.all()
+            tags_results = list(
+                map(lambda tag: tag.to_dict(), tags_results)
+            )
+            return {"tags": tags_results}
+        except Exception as postgres_error:
+            raise postgres_error
+
