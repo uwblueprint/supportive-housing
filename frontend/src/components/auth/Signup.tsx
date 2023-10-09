8597db0ce94619c91671f8cd2fc01aa27385e2ce
Aathithan Chandrabalan <a69chand@uwaterloo.ca>
2023-10-09 19:23:09 -0400

    init

--- frontend/src/components/auth/Signup.tsx
+++ frontend/src/components/auth/Signup.tsx
@@ -1,4 +1,11 @@
 import React, { useContext, useState } from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  Text,
+} from "@chakra-ui/react";
 import { Redirect } from "react-router-dom";
 
 import authAPIClient from "../../APIClients/AuthAPIClient";
@@ -13,6 +20,7 @@ const Signup = (): React.ReactElement => {
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  // const [passwordError, setPasswordError]
 
   const onSignupClick = async () => {
     const isInvited = await commonApiClient.isUserInvited(email);
@@ -37,52 +45,9 @@ const Signup = (): React.ReactElement => {
   }
 
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>Signup</h1>
-      <form>
-        <div>
-          <input
-            type="text"
-            value={firstName}
-            onChange={(event) => setFirstName(event.target.value)}
-            placeholder="first name"
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            value={lastName}
-            onChange={(event) => setLastName(event.target.value)}
-            placeholder="last name"
-          />
-        </div>
-        <div>
-          <input
-            type="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            placeholder="username@domain.com"
-          />
-        </div>
-        <div>
-          <input
-            type="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            placeholder="password"
-          />
-        </div>
-        <div>
-          <button
-            className="btn btn-primary"
-            type="button"
-            onClick={onSignupClick}
-          >
-            Sign Up
-          </button>
-        </div>
-      </form>
-    </div>
+    <Box>
+      {/* init */}
+    </Box>
   );
 };
 
