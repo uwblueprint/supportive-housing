11ac4808fdf229b8ddf9bf7e7f85f5ad357dc0d3
owen-sellner <owen.sellner@gmail.com>
2023-10-15 16:32:39 -0400

    add hidden input

--- frontend/src/components/auth/Authy.tsx
+++ frontend/src/components/auth/Authy.tsx
@@ -1,5 +1,13 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useRef } from "react";
 import { Redirect } from "react-router-dom";
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  Text,
+  VStack
+} from "@chakra-ui/react";
 import authAPIClient from "../../APIClients/AuthAPIClient";
 import AUTHENTICATED_USER_KEY from "../../constants/AuthConstants";
 import { HOME_PAGE } from "../../constants/Routes";
@@ -23,6 +31,8 @@ const Authy = ({
   const [error, setError] = useState("");
   const [passcode, setPasscode] = useState("");
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const onAuthySubmit = async () => {
     let authUser: AuthenticatedUser | null;
 
@@ -40,40 +50,59 @@ const Authy = ({
     }
   };
 
+  const handlePasscode = (e: { target: { value: unknown } }) => {
+    const newPasscode = e.target.value as string;
+    if (newPasscode !== null && /^[0-9]{0,6}$/.test(newPasscode)) {
+      setPasscode(newPasscode);
+    }
+  };
+
+  const handleFocus = () => {
+    if (inputRef && inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
   if (authenticatedUser) {
     return <Redirect to={HOME_PAGE} />;
   }
 
   if (toggle) {
     return (
-      <div style={{ textAlign: "center" }}>
-        <h1>Authy</h1>
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            onAuthySubmit();
-          }}
-        >
-          <div>
-            <p>Enter token from Authy App</p>
-            <input
-              type="text"
-              value={passcode}
-              onChange={(event) => {
-                setPasscode(event.target.value);
-                setError("");
-              }}
-              placeholder="passcode"
-            />
-          </div>
-          <div>
-            <button className="btn btn-primary" type="submit">
-              Verify
-            </button>
-          </div>
-          {error && <p style={{ color: "red" }}>{error}</p>}
-        </form>
-      </div>
+      <>
+        <Box bg="teal.400" height="100vh" onClick={handleFocus}>
+          <Flex bg="white" height="100vh" width="47%" justifyContent="center" alignItems="center">
+            <VStack width="77%" border="2px solid red" align="flex-start" gap="2vh">
+              <Text variant="login">One last step!</Text>
+              <Text variant="loginSecondary">In order to protect your account, please confirm the authorization code sent to {email}</Text>
+              <Button
+                variant="login"
+                disabled={email === '' || password === ''}
+                _hover={
+                  email && password
+                    ? {
+                      background: "teal.500",
+                      transition:
+                        "transition: background-color 0.5s ease !important",
+                    }
+                    : {}
+                }
+              >
+                Log In
+              </Button>
+              <h1>{passcode}</h1>
+              <Input
+                ref={inputRef}
+                autoFocus
+                position="absolute"
+                left="-999999px"
+                value={passcode}
+                onChange={handlePasscode}
+              />
+            </VStack>
+          </Flex>
+        </Box>
+      </>
     );
   }
   return <></>;
