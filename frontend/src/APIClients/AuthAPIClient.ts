6dc13d556b12f4a0ca9bf3302d164248594c0575
Carolyn Zhang <carolyn.zhang18@gmail.com>
2023-10-15 15:33:38 -0400

    Remove localStorage token

--- frontend/src/APIClients/AuthAPIClient.ts
+++ frontend/src/APIClients/AuthAPIClient.ts
@@ -103,7 +103,6 @@ const register = async (
       { firstName, lastName, email, password },
       { withCredentials: true },
     );
-    localStorage.setItem(AUTHENTICATED_USER_KEY, JSON.stringify(data));
     return data;
   } catch (error) {
     return null;
