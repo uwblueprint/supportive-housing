d3cd1163874972377b62bb43d4741bc41ce15325
Aathithan Chandrabalan <75588919+achandrabalan@users.noreply.github.com>
2023-10-15 16:36:52 -0400

    Signup Page Skeleton (#178)
    * init
    
    * Added components and connections

--- frontend/src/components/auth/Signup.tsx
+++ frontend/src/components/auth/Signup.tsx
@@ -1,8 +1,14 @@
 import React, { useContext, useState } from "react";
-import { Redirect } from "react-router-dom";
-
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+import { Redirect, useHistory } from "react-router-dom";
 import authAPIClient from "../../APIClients/AuthAPIClient";
-import { HOME_PAGE } from "../../constants/Routes";
+import { HOME_PAGE, LOGIN_PAGE } from "../../constants/Routes";
 import AuthContext from "../../contexts/AuthContext";
 import { AuthenticatedUser } from "../../types/AuthTypes";
 import commonApiClient from "../../APIClients/CommonAPIClient";
@@ -13,6 +19,8 @@ const Signup = (): React.ReactElement => {
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const history = useHistory();
+  // const [passwordError, setPasswordError]
 
   const onSignupClick = async () => {
     const isInvited = await commonApiClient.isUserInvited(email);
@@ -32,57 +40,116 @@ const Signup = (): React.ReactElement => {
     }
   };
 
+  const onLogInClick = () => {
+    history.push(LOGIN_PAGE);
+  }; 
+
   if (authenticatedUser) {
     return <Redirect to={HOME_PAGE} />;
   }
-
+  // Lock scroll
+  document.body.style.overflow = "hidden"
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>Signup</h1>
-      <form>
-        <div>
-          <input
-            type="text"
-            value={firstName}
-            onChange={(event) => setFirstName(event.target.value)}
-            placeholder="first name"
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            value={lastName}
-            onChange={(event) => setLastName(event.target.value)}
-            placeholder="last name"
-          />
-        </div>
-        <div>
-          <input
-            type="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            placeholder="username@domain.com"
-          />
-        </div>
-        <div>
-          <input
-            type="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            placeholder="password"
-          />
-        </div>
-        <div>
-          <button
-            className="btn btn-primary"
-            type="button"
-            onClick={onSignupClick}
+    <Flex h="100vh">
+        <Box w="47%">
+          <Flex
+            display="flex"  
+            alignItems="flex-start"
+            position="absolute"
+            top="17.5%"
+            left="6%"
+            w="100%"
           >
-            Sign Up
-          </button>
-        </div>
-      </form>
-    </div>
+            <Text variant="login" position="absolute">
+              Sign Up
+            </Text>
+          </Flex>
+          <Flex
+            h="40%"
+            w="36%"
+            top="28%"
+            left="6%"
+            direction="column"
+            position="absolute"
+            justifyContent="space-between"
+          >
+            <Box>
+              <Input 
+                variant="login"
+                position="absolute"
+                placeholder="Your first name"
+                value={firstName}
+                onChange={(event) => setFirstName(event.target.value)}
+              />
+            </Box>
+            <Box>
+              <Input 
+                variant="login"
+                position="absolute"
+                placeholder="Your last name"
+                value={lastName}
+                onChange={(event) => setLastName(event.target.value)}
+              />
+            </Box>
+            <Box>
+              <Input 
+                variant="login"
+                position="absolute"
+                placeholder="Your email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </Box>
+            <Box>
+              <Input 
+                variant="login"
+                type="password"
+                position="absolute"
+                placeholder="Your password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </Box>
+            <Box>
+              <Button
+                variant="login"
+                position="absolute"
+                disabled={email === '' || password === '' || firstName === '' || lastName === ''}
+                _hover={
+                  email && password && firstName && lastName
+                    ? {
+                        background: "teal.500",
+                        transition:
+                          "transition: background-color 0.5s ease !important",
+                      }
+                    : {}
+                }
+                onClick={onSignupClick}
+              >
+                Create Account
+              </Button>
+            </Box>
+          </Flex>
+          <Flex
+            top="80%"
+            left="6%"
+            width="100%"
+            direction="row"
+            position="absolute"
+            alignContent="center"
+          >
+            <Text variant="loginSecondary" paddingRight="1.1%">
+              Already have an account?
+            </Text>
+            <Text variant="loginTertiary" onClick={onLogInClick}>
+              Log In Now
+            </Text>
+          </Flex>
+        </Box>
+        <Box flex="1" bg="teal.400">
+          {/* Background */}
+        </Box>
+    </Flex>
   );
 };
 
