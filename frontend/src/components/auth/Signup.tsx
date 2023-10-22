9abe0bf62e0ea4bdc820bbe9e53e939df1af6c27
Kevin Pierce <kevinpierce08@gmail.com>
2023-10-22 17:05:18 -0400

    Fixed styling issues

--- frontend/src/components/auth/Signup.tsx
+++ frontend/src/components/auth/Signup.tsx
@@ -66,109 +66,172 @@ const Signup = (): React.ReactElement => {
   document.body.style.overflow = "hidden"
   return (
     <Flex h="100vh">
-        <Box w="47%">
+      <Box w="47%">
+        <Flex marginTop="172px" display="flex" align="center" justify="center">
           <Flex
-            display="flex"  
-            alignItems="flex-start"
-            position="absolute"
-            top="17.5%"
-            left="6%"
-            w="100%"
+            width="76%"
+            align="flex-start"
+            direction="column"
+            gap="28px"
           >
-            <Text variant="login" position="absolute">
+            <Text variant="login" paddingBottom="12px">
               Sign Up
             </Text>
-          </Flex>
-          <Flex
-            h="40%"
-            w="36%"
-            top="28%"
-            left="6%"
-            direction="column"
-            position="absolute"
-            justifyContent="space-between"
-          >
-            <Box>
+            <Input 
+              variant="login"
+              placeholder="Your first name"
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
+            />
+            <Input 
+              variant="login"
+              placeholder="Your last name"
+              value={lastName}
+              onChange={(event) => setLastName(event.target.value)}
+            />
+            <FormControl isRequired isInvalid={emailError}>
               <Input 
                 variant="login"
-                position="absolute"
-                placeholder="Your first name"
-                value={firstName}
-                onChange={(event) => setFirstName(event.target.value)}
+                placeholder="Your email"
+                value={email}
+                onChange={handleEmailChange}
               />
-            </Box>
-            <Box>
+              <FormErrorMessage>Please enter a valid email.</FormErrorMessage>
+            </FormControl>
+            <FormControl isRequired>
               <Input 
                 variant="login"
-                position="absolute"
-                placeholder="Your last name"
-                value={lastName}
-                onChange={(event) => setLastName(event.target.value)}
+                type="password"
+                placeholder="Your password"
+                value={password}
+                onChange={handlePasswordChange}
               />
-            </Box>
-            <Box>
-              <FormControl isRequired isInvalid={emailError}>
-                <Input 
-                  variant="login"
-                  position="absolute"
-                  placeholder="Your email"
-                  value={email}
-                  onChange={handleEmailChange}
-                />
-                <FormErrorMessage>Please enter a valid email.</FormErrorMessage>
-              </FormControl>
-            </Box>
-            <Box>
-              <FormControl isRequired>
-                <Input 
-                  variant="login"
-                  type="password"
-                  position="absolute"
-                  placeholder="Your password"
-                  value={password}
-                  onChange={handlePasswordChange}
-                />
-              </FormControl>
-            </Box>
-            <Box>
-              <Button
+            </FormControl>
+
+            <Button
+              variant="login"
+              disabled={email === '' || password === '' || firstName === '' || lastName === ''}
+              _hover={
+                email && password && firstName && lastName
+                  ? {
+                      background: "teal.500",
+                      transition:
+                        "transition: background-color 0.5s ease !important",
+                    }
+                  : {}
+              }
+              onClick={onSignupClick}
+            >
+              Create Account
+            </Button>
+            <Flex
+              paddingTop="29px"
+              alignContent="center"
+            >
+              <Text variant="loginSecondary" paddingRight="17px">
+                Already have an account?
+              </Text>
+              <Text variant="loginTertiary" onClick={onLogInClick}>
+                Log In Now
+              </Text>
+            </Flex>
+          </Flex>
+        </Flex>
+        
+        {/* <Flex
+          display="flex"  
+          alignItems="flex-start"
+          top="17.5%"
+          left="6%"
+          w="100%"
+        >
+          <Text variant="login" position="absolute">
+            Sign Up
+          </Text>
+        </Flex>
+        <Flex
+          h="40%"
+          w="36%"
+          top="28%"
+          left="6%"
+          direction="column"
+          justifyContent="space-between"
+        >
+          <Box>
+            <Input 
+              variant="login"
+              placeholder="Your first name"
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
+            />
+          </Box>
+          <Box>
+            <Input 
+              variant="login"
+              placeholder="Your last name"
+              value={lastName}
+              onChange={(event) => setLastName(event.target.value)}
+            />
+          </Box>
+          <Box>
+            <FormControl isRequired isInvalid={emailError}>
+              <Input 
                 variant="login"
-                position="absolute"
-                disabled={email === '' || password === '' || firstName === '' || lastName === ''}
-                _hover={
-                  email && password && firstName && lastName
-                    ? {
-                        background: "teal.500",
-                        transition:
-                          "transition: background-color 0.5s ease !important",
-                      }
-                    : {}
-                }
-                onClick={onSignupClick}
-              >
-                Create Account
-              </Button>
-            </Box>
-          </Flex>
-          <Flex
-            top="80%"
-            left="6%"
-            width="100%"
-            direction="row"
-            position="absolute"
-            alignContent="center"
-          >
-            <Text variant="loginSecondary" paddingRight="1.1%">
-              Already have an account?
-            </Text>
-            <Text variant="loginTertiary" onClick={onLogInClick}>
-              Log In Now
-            </Text>
-          </Flex>
-        </Box>
-        <Box flex="1" bg="teal.400">
-          {/* Background */}
-        </Box>
+                placeholder="Your email"
+                value={email}
+                onChange={handleEmailChange}
+              />
+              <FormErrorMessage>Please enter a valid email.</FormErrorMessage>
+            </FormControl>
+          </Box>
+          <Box>
+            <FormControl isRequired>
+              <Input 
+                variant="login"
+                type="password"
+                placeholder="Your password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </FormControl>
+          </Box>
+          <Box>
+            <Button
+              variant="login"
+              disabled={email === '' || password === '' || firstName === '' || lastName === ''}
+              _hover={
+                email && password && firstName && lastName
+                  ? {
+                      background: "teal.500",
+                      transition:
+                        "transition: background-color 0.5s ease !important",
+                    }
+                  : {}
+              }
+              onClick={onSignupClick}
+            >
+              Create Account
+            </Button>
+          </Box>
+        </Flex>
+        <Flex
+          top="80%"
+          left="6%"
+          width="100%"
+          direction="row"
+          alignContent="center"
+        >
+          <Text variant="loginSecondary" paddingRight="1.1%">
+            Already have an account?
+          </Text>
+          <Text variant="loginTertiary" onClick={onLogInClick}>
+            Log In Now
+          </Text>
+        </Flex> */}
+      </Box>
+      <Box flex="1" bg="teal.400">
+        {/* Background */}
+      </Box>
     </Flex>
   );
 };
