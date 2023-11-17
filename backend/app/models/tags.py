f912d507b01483b4763147754aa7f5fcfe227c59
Carolyn Zhang <82423082+carolynzhang18@users.noreply.github.com>
2023-11-17 16:52:05 -0500

    Create tags route (#197)
    * Add unique constraint to tag name
    
    * Create tag route
    
    * Modify update route to catch IntegrityError
    
    * Add name for migration downgrade

--- backend/app/models/tags.py
+++ backend/app/models/tags.py
@@ -8,7 +8,7 @@ class Tag(db.Model):
     __tablename__ = "tags"
 
     tag_id = db.Column(db.Integer, primary_key=True, nullable=False)
-    name = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
     status = db.Column(db.Enum("Deleted", "Active", name="status"), nullable=False)
     log_records = db.relationship(
         "LogRecords", secondary="log_record_tag", back_populates="tags"
