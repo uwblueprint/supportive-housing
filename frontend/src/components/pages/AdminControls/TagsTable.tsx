857b6555cd74bce328776a3d5a59402a0b2694ea
Carolyn Zhang <carolyn.zhang18@gmail.com>
2023-12-03 15:51:08 -0500

    Complete Tags page frontend design

--- frontend/src/components/pages/AdminControls/TagsTable.tsx
+++ frontend/src/components/pages/AdminControls/TagsTable.tsx
@@ -1,16 +1,24 @@
 import React, { RefObject } from "react";
 import {
   Box,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
   Table,
   TableContainer,
   Tbody,
+  Td,
   Th,
   Thead,
   Tr,
 } from "@chakra-ui/react";
+import { VscKebabVertical } from "react-icons/vsc";
+import { Tag } from "../../../types/TagTypes";
 
 type Props = {
-  /* TODO: tags: Tag[]; */
+  tags: Tag[];
   tableRef: RefObject<HTMLDivElement>;
   userPageNum: number;
   setUserPageNum: React.Dispatch<React.SetStateAction<number>>;
@@ -19,7 +27,7 @@ type Props = {
 };
 
 const TagsTable = ({
-  /* TODO: tags, */
+  tags,
   tableRef,
   userPageNum,
   setUserPageNum,
@@ -40,8 +48,37 @@ const TagsTable = ({
               <Th>
                 Tag Name
               </Th>
+              <Th> </Th>
             </Tr>
           </Thead>
+          <Tbody>
+            {tags.map((tag) => {
+              return (
+                <Tr key={tag.tagId} verticalAlign="middle">
+                  <Td>{tag.name}</Td>
+                  <Td width="1%">
+                    <Menu>
+                      <MenuButton
+                        as={IconButton}
+                        aria-label="Options"
+                        icon={<VscKebabVertical />}
+                        w="36px"
+                        variant="ghost"
+                      />
+                      <MenuList>
+                        <MenuItem>
+                          Edit Tag
+                        </MenuItem>
+                        <MenuItem>
+                          Delete Tag
+                        </MenuItem>
+                      </MenuList>
+                    </Menu>
+                  </Td>
+                </Tr>
+              );
+            })}
+          </Tbody>
         </Table>
       </TableContainer>
     </Box>
